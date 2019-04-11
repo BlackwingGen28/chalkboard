a217f31c36ddbd40e5744c60443bdfce2c897a8d
Jordan Morgan <jordan.morgan2@smail.astate.edu>
2019-04-11 14:56:03 -0500

    done

--- assignment6_in_class/ViewController.swift
+++ assignment6_in_class/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var numberOfReps :Int = 0
+    
+    
+    @IBOutlet weak var stepperLabel: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var sentence: UITextField!
+    @IBOutlet weak var chalkboard: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func executeButtonPressed(_ sender: Any) {
+        chalkboard.text = ""
+        if(sentence.text != ""){//check if sentence field has a value
+            if(numberOfReps > 0){//check if we have a number of reps that is 1 or more
+                for i in 0...(numberOfReps-1){
+                    chalkboard.text += sentence.text!
+                    chalkboard.text += "\n"
+                }
+                numberOfReps = 0
+                stepper.value = 0
+                stepperLabel.text = "0"
+            }
+        }
+    }
+    
+    
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        numberOfReps = Int(stepper.value)
+        stepperLabel.text = "\(numberOfReps)"
+    }
+    
 }
 
